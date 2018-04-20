c7acb0dc14eb77e792e2f41fcd3f040999941893
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-19 21:41:01 -0400

    Last commit for the night

--- liri.js
+++ liri.js
@@ -1,7 +1,37 @@
-
+//Required Variables
 require("dotenv").config()
 
 let keys = require(keys.js);
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+//Global Variables
+let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+
+let action = process.argv[2];
+let input = process.argv[3];
+
+
+//Switch statement To Turn Node.js input into action
+switch (action) {
+    case "my-tweets":
+        myTweets();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+}
+
+//Twitter function
+
+function myTweets() {
+    
+}
