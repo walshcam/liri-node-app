d8fdd31baabc2253ee9d15aa01bf6c50681b2ba3
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-22 08:59:46 -0400

    Nothing works but Twitter and Spotify are set up

--- liri.js
+++ liri.js
@@ -1,15 +1,28 @@
 //Required Variables
 require("dotenv").config()
 
-let keys = require(keys.js);
+let keys = require('./keys.js');
+let Twitter = require('twitter');
+let Spotify = require('node-spotify-api');
 
 //Global Variables
-let spotify = new Spotify(keys.spotify);
-let client = new Twitter(keys.twitter);
+// let spotify = new Spotify(keys.spotify);
 
 let action = process.argv[2];
-let input = process.argv[3];
 
+//Twitter required variables
+let client = new Twitter({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token_key: keys.access_token_key,
+    access_token_secret: keys.access_token_secret
+  });
+
+//Spotify required variables
+let spotify = new Spotify({
+    id: keys.id,
+    secret: keys.secret
+  });
 
 //Switch statement To Turn Node.js input into action
 switch (action) {
@@ -30,8 +43,64 @@ switch (action) {
         break;
 }
 
-//Twitter function
+//Twitter Function
 
 function myTweets() {
+
+    let params = {screen_name: 'milkisspicy',
+                  count: "20"
+    };
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (error) {
+          console.log(error)
+      }
+      if (!error) {
+        console.log("At least I got here")
+        console.log(tweets);
+      }
+    });
+};
+
+//Spotify Function
+
+function spotifyThisSong() {
     
-}
+    let query;
+
+    if (process.argv[3] === undefined) {
+        query = 'The sign by Ace of Base'
+    }
+    else {
+        for (let i = 3; i < process.argv.length; i++) {
+            query = query + " " + process.argv[i];
+        }
+        console.log(query)
+    }
+
+    let params = {
+        type: "track",
+        query: query
+    }
+    spotify.search(params, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        if (!err){
+            console.log(data); 
+        }
+    });
+
+};
+
+//OMBD Function
+
+function movieThis() {
+
+};
+
+//Do What It Says
+
+function doWhatItSays() {
+
+};
