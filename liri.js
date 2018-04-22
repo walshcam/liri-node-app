4509dbe7586b6535524b525964af08024cb315c0
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-22 09:48:39 -0400

    Set up OMBD. Need to look into object to make sure rotton tomato rating is correct

--- liri.js
+++ liri.js
@@ -123,14 +123,21 @@ function movieThis() {
         console.log(query)
     }
 
-    request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    request(`http://www.omdbapi.com/?t=${query}&y=&plot=short&apikey=trilogy`, function(error, response, body) {
 
         // If the request is successful (i.e. if the response status code is 200)
         if (!error && response.statusCode === 200) {
       
           // Parse the body of the site and recover just the imdbRating
           // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-          console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+          console.log("The movie's title is: " + JSON.parse(body).Title);
+          console.log("The year this movie was released is: " + JSON.parse(body).Year);
+          console.log("The movie's IMDB rating is: " + JSON.parse(body).imdbRating);
+          console.log("The movie's Rotton Tomatoes rating is: " + JSON.parse(body).Ratings[2]);
+          console.log("The movie was produced in: " + JSON.parse(body).Country);
+          console.log("The language this movie was made in: " + JSON.parse(body).Language);
+          console.log("Plot: " + JSON.parse(body).Plot);
+          console.log("The movie's actors are: " + JSON.parse(body).Actors);
         }
       });
 };
